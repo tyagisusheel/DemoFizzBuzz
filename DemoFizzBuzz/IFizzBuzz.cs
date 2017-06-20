98d737a0a9509e374034889f9112f38c41f5a736
tyagisusheel <tyagisusheel@gmail.com>
2017-06-20 13:06:59 +0100

    Created Interface "IFizzBuzz"
    Created the Interface for parsing the number for FizzBuzz

---
+++ DemoFizzBuzz/IFizzBuzz.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoFizzBuzz
+{
+
+    // Interface for parsing the number for FizzBuzz
+    public interface IFizzBuzz
+    {
+        /// <summary>
+        /// Defination ParseNumber method for parse the number for  FizzBuzz
+        /// </summary>
+        /// <param name="number"> Valid numeric value for parset </param>
+        /// <param name="result"> String input as a return type. Default is empty </param>
+        string ParseNumber(int number, string result = "");
+    }
+}
--- DemoFizzBuzz/Program.cs
+++ DemoFizzBuzz/Program.cs
@@ -1,4 +1,15 @@
-﻿using System;
+﻿// ===============================
+// AUTHOR           : Susheel Tyagi
+// CREATE DATE      : 20/06/2017
+// PURPOSE          : Demo Application for Test ability
+// SPECIAL NOTES    : This is Console Based Application for FizzBuzz with Dot.Net best practices, Software Development 
+//                    best practices, Test Driven Development Best Practices and Source Control Best Practices.
+// ===============================
+// Change History: 1.0
+//
+//==================================
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
